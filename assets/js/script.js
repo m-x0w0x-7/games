fbca19b52cb3b7260d2e34dab8c9bcd9802ac350
MiO <m.x0w0x.7@gmail.com>
2026-04-03 00:29:22 +0900

    Add: privacy policy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('top');
+  // contact account
+  function account() {
+    const u = 'mio';
+    const d = 'lunamare';
+
+    const target = document.querySelector('.contact_account');
+    const link = 'https://x.com/' + u + '_' + d;
+    const text = '@' + u + '_' + d;
+
+    const a = document.createElement('a');
+    a.setAttribute('href', link);
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+    a.textContent = text;
+    target.append(a);
+  }
+  account();
 });
